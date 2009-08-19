3686b2227ef15e959ec1cd55b23bd162e8b4d9d7
Jon Works <faux@fauxsoft.com>
2009-08-19 06:33:24 -0400

    allow signing of update files without rebuilding

--- make-update-zip.sh
+++ make-update-zip.sh
@@ -3,18 +3,48 @@ WD=$(pwd)
 echo WD=${WD}
 OUT=${ANDROID_PRODUCT_OUT}
 
-echo cleaning old files
-rm -v update.zip update-signed.zip
 
-cd ${OUT}
+add_tag(){
+    TAG=$1
+    FILE=$2
 
-echo building update.zip
-cat <(find {META-INF,system,data}/ -type d) <(find {META-INF,system,data}/ -type f) <(echo boot.img) | zip -@ ${WD}/update.zip
+    D=$(dirname ${FILE})
+    BN=$(basename ${FILE})
+    EXT=$(sed 's/^.*\.//' <<< ${BN})
+    BN=$(sed "s/.${EXT}$//" <<< ${BN})
+    echo "${D}/${BN}-signed.${EXT}"
+}
 
-cd ${WD}
-echo ${WD}
-echo $(pwd)
+sign_file(){
+    TAG=signed
+    FILE=$1
+    OUT_FILE=$(add_tag ${TAG} ${FILE})
+    echo "Signing: $FILE -> $OUT_FILE"
+    java -jar ${ANDROID_BUILD_TOP}/out/host/linux-x86/framework/signapk.jar ${ANDROID_BUILD_TOP}/build/target/product/security/testkey.x509.pem ${ANDROID_BUILD_TOP}/build/target/product/security/testkey.pk8 ${FILE} ${OUT_FILE}
+}
 
-echo signing update.zip "->" update-signed.zip
-java -jar ${ANDROID_BUILD_TOP}/out/host/linux-x86/framework/signapk.jar ${ANDROID_BUILD_TOP}/build/target/product/security/testkey.x509.pem ${ANDROID_BUILD_TOP}/build/target/product/security/testkey.pk8 update.zip update-signed.zip
+case "$1" in
+    sign)
+        shift
+        sign_file $*
+        ;;
+    package)
+        echo cleaning old files
+        rm -v update.zip update-signed.zip
+
+        cd ${OUT}
+
+        echo building update.zip
+        cat <(find {META-INF,system,data}/ -type d) <(find {META-INF,system,data}/ -type f) <(echo boot.img) | zip -@ ${WD}/update.zip
+
+        cd ${WD}
+        echo ${WD}
+        echo $(pwd)
+
+        echo signing update.zip "->" update-signed.zip
+        ;;
+    *)
+        usage
+        ;;
+esac
 
